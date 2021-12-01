d30db6b363df9a6e7f897dd4f1d9f398c49d456e
zhanglikun <iuxt@qq.com>
2021-12-01 15:46:50 +0800

    add add_user option

--- openldap/exec_basedomain.sh
+++ openldap/exec_basedomain.sh
@@ -3,3 +3,9 @@ expect {
 "Enter LDAP Password:" {send "com.012\n";exp_continue}
 eof
 }
+
+spawn ldapadd -x -D cn=Manager,dc=nutstore,dc=com -W -f /vagrant/add_user.ldif
+expect {
+"Enter LDAP Password:" {send "com.012\n";exp_continue}
+eof
+}
