5bbbe1bf9ef1247f1212db0e4482f8ebc33eae2b
zhanglikun <iuxt@qq.com>
2021-12-01 16:52:03 +0800

    add test ldap

--- radius/radtest.sh
+++ radius/radtest.sh
@@ -1,4 +1,9 @@
+# test local
 radtest bob com.012 localhost:1812 0 testing123
+
+# test ldap
+radtest zhanglikun 123456 localhost:1833 0 testing123
+
 # radtest <username> <password> radius_server:1812 0 <secret>
 # username and password can use ldap
 # secret in clients.conf
